48130415821c8d4718e44937cb5ea2415e25fbf0
EC2 Default User <ec2-user@ip-172-31-1-198.us-west-2.compute.internal>
2019-05-06 22:41:08 +0000

    updated build items

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ go get  "github.com/lib/pq"
 go get  "github.com/davecgh/go-spew/spew"
 go get  "github.com/bitly/go-simplejson"
 go get  "github.com/akkeris/vault-client"
+go get  "github.com/aws/aws-sdk-go/aws"
+go get  "github.com/aws/aws-sdk-go/aws/awserr"
+go get  "github.com/aws/aws-sdk-go/aws/session"
+go get  "github.com/aws/aws-sdk-go/service/s3"
 cd /go/src/taas
 go build server.go
 
