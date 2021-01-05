6d51244f88893d2ebaf5ec7e93e3df58b324d666
Calaway <calaway@users.noreply.github.com>
2021-01-05 14:53:37 -0700

    COBRA-3975: Error handling

--- migrations/released.go
+++ migrations/released.go
@@ -14,31 +14,35 @@ func MigrateToReleased() {
 	var releaseDiagnostics []structs.DiagnosticSpec
 	db, err := sql.Open("postgres", os.Getenv("DIAGNOSTICDB"))
 	if err != nil {
+		fmt.Println("Error opening DB")
 		panic(err)
 	}
 	defer db.Close()
 	rows, err := db.Query("SELECT app, space, action, job, jobspace FROM diagnostics WHERE action=$1", "release")
 	if err != nil {
-		panic(err)
+		fmt.Println("Error selecting all diagnostics with action=release")
+		fmt.Println(err)
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var releaseDiagnostic structs.DiagnosticSpec
 		err = rows.Scan(&releaseDiagnostic.App, &releaseDiagnostic.Space, &releaseDiagnostic.Action, &releaseDiagnostic.Job, &releaseDiagnostic.JobSpace)
 		if err != nil {
-			panic(err)
+			fmt.Println("Error scanning rows")
+			fmt.Println(err)
 		}
 		releaseDiagnostics = append(releaseDiagnostics, releaseDiagnostic)
 	}
 	if err = rows.Err(); err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 
 	for _, releaseDiagnostic := range releaseDiagnostics {
 		appspace := releaseDiagnostic.App + "-" + releaseDiagnostic.Space
 		hooks, err := jobs.GetHooks(appspace)
 		if err != nil {
-			panic(err)
+			fmt.Println("Error: Unable to get hook. Diagnostic " + releaseDiagnostic.Job + "-" + releaseDiagnostic.JobSpace + ". App: " + appspace)
+			fmt.Println(err)
 		}
 
 		for _, hook := range hooks {
@@ -46,7 +50,8 @@ func MigrateToReleased() {
 				fmt.Println("Deleting release webhook:\n  App: " + appspace + "\n  ID:  " + hook.ID + "\n  URL: " + hook.URL + "\n")
 				err := jobs.DeleteHook(appspace, hook.ID)
 				if err != nil {
-					panic(err)
+					fmt.Println("Error: Unable to delete hook. Diagnostic " + releaseDiagnostic.Job + "-" + releaseDiagnostic.JobSpace + ". App: " + appspace + " Hook ID: " + hook.ID + ". Hook URL: " + hook.URL)
+					fmt.Println(err)
 				}
 			}
 		}
@@ -60,7 +65,8 @@ func MigrateToReleased() {
 			WHERE job=$1 and jobspace=$2`
 		_, err = db.Exec(sqlStatement, releaseDiagnostic.Job, releaseDiagnostic.JobSpace)
 		if err != nil {
-			panic(err)
+			fmt.Println("Error: Unable to update diagnostic. Diagnostic " + releaseDiagnostic.Job + "-" + releaseDiagnostic.JobSpace + ". App: " + appspace)
+			fmt.Println(err)
 		}
 	}
 }
